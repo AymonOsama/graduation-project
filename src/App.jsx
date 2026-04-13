8c7a1c8a5d98bd566429d6d37c7cc7bfa4a8edfe
AymonOsama <Ayyman834@gmail.com>
2026-04-13 14:18:15 +0200

    initial project setup with tailwind and framer motion

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen bg-blue-600 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold p-10 border-4 border-white rounded-xl">
+        لو الخلفية زرقاء يبقى كلو تمام! ✌️
+      </h1>
+    </div>
+  )
+}
+export default App
